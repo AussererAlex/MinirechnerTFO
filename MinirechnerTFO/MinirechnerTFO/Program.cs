7f7daef15f0aa60d225d44c61306a219438ea36a
AussererAlex <63018989+AussererAlex@users.noreply.github.com>
2020-04-23 13:27:15 +0200

    Main vorbereitung

---
+++ MinirechnerTFO/MinirechnerTFO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinirechnerTFO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  + Minirechner +");
+            Console.Write("\nGeben Sie Zahl1 ein: ");
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Geben Sie Zahl2 ein: ");
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+            
+        }
+    }
+}
