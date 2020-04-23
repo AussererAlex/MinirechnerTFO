02b18e73057ab0a4c2e833be240da4c08d030728
AussererAlex <63018989+AussererAlex@users.noreply.github.com>
2020-04-23 13:45:31 +0200

    Suptraktion

--- MinirechnerTFO/MinirechnerTFO/Program.cs
+++ MinirechnerTFO/MinirechnerTFO/Program.cs
@@ -17,9 +17,11 @@ namespace MinirechnerTFO
             int Zahl2 = Convert.ToInt32(Console.ReadLine());
 
             //Ergebnisse
-
+            Console.WriteLine("----------------------");
             int ErgA = Addition(Zahl1, Zahl2);
             Console.WriteLine("\nDas Ergebniss der Addition beträgt: "+ErgA);
+            int ErgS = Suptraktion(Zahl1, Zahl2);
+            Console.WriteLine("Das Ergebniss der Suptraktion beträgt: " + ErgS);
 
             Console.ReadKey();
 
@@ -28,6 +30,11 @@ namespace MinirechnerTFO
         public static int Addition(int Zahl1, int Zahl2)
         {
             int Erg = Zahl1 + Zahl2;
+            return Erg;
+        }
+        public static int Suptraktion(int Zahl1, int Zahl2)
+        {
+            int Erg = Zahl1 - Zahl2;
             return Erg;
         }
     }
